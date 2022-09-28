e0af9bdff44e1a7e2732bd3964311c73058eed60
andrew <andrewmoren93@gmail.com>
2022-09-28 15:48:26 +0300

    add button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 
 import { Link, useLocation } from "react-router-dom";
 
@@ -17,7 +17,22 @@ const Header = () => {
   const headerRef = useRef(null);
 
   const active = headerNav.findIndex((e) => e.path === pathname);
-
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
   return (
     <div ref={headerRef} className="header">
       <div className="header__wrap container">
